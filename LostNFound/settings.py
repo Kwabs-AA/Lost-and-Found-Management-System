3216294b9a7568a086bacacd6db1a62f0ca51af4
Kwabs-AA <kwabenaansah100@gmail.com>
2024-08-24 20:49:57 +0000

    requirement changes

--- LostNFound/settings.py
+++ LostNFound/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,6 +92,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Inventory', 'static')]
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#!/bin/bash
-
-pip3 install -r requirements.txt  # Use pip3 instead of pip
-python3 manage.py collectstatic --noinput  # Use python3 instead of python
-python3 manage.py migrate  # Use python3 instead of python
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
