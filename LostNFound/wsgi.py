725354254836e90c8101305f1e32eac5e65fec95
Kwabs-AA <kwabenaansah100@gmail.com>
2024-08-24 04:05:59 +0000

    requirement changes

--- LostNFound/wsgi.py
+++ LostNFound/wsgi.py
@@ -1,3 +1,8 @@
+"""
+WSGI config for LostNFound project.
+
+It exposes the WSGI callable as a module-level variable named `application`.
+"""
 import os
 from django.core.wsgi import get_wsgi_application
 
