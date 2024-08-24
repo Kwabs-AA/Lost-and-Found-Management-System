ecd12fc7ea252ff13ffaf35948d95075466f602a
Kwabs-AA <kwabenaansah100@gmail.com>
2024-08-24 10:48:58 +0000

    requirement change

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
-# build_files.sh
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+#!/bin/bash
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput
