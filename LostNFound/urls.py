bf2eeb7afdf0401b725c20e11f11f61f7509487c
Kwabs-AA <kwabenaansah100@gmail.com>
2024-08-24 03:20:14 +0000

    settings changes

--- LostNFound/urls.py
+++ LostNFound/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Inventory.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('Inventory.urls')),  # Ensure this is pointing to the correct app URLs
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
